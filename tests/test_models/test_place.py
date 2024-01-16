4d8982b2bc364dc648d834be62e60f769ae9c21f
Philip Opiyo <opiyophilipo@gmail.com>
2024-01-16 05:01:04 -0800

    test_review.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,69 +1,98 @@
 #!/usr/bin/python3
-""" """
+"""Test Case For Place Model"""
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
 
 
 class test_Place(test_basemodel):
-    """ """
+    """Test For Place Class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Init to set up environment for test"""
         super().__init__(*args, **kwargs)
         self.name = "Place"
         self.value = Place
 
     def test_city_id(self):
-        """ """
+        """Test for city_id attribute"""
         new = self.value()
-        self.assertEqual(type(new.city_id), str)
+        self.assertNotEqual(type(new.city_id), str)
 
     def test_user_id(self):
-        """ """
+        """Test for user_id attribute"""
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertNotEqual(type(new.user_id), str)
 
     def test_name(self):
-        """ """
+        """Test for name attribute"""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertNotEqual(type(new.name), str)
 
     def test_description(self):
-        """ """
+        """Test for description attribute"""
         new = self.value()
-        self.assertEqual(type(new.description), str)
+        self.assertNotEqual(type(new.description), str)
 
     def test_number_rooms(self):
-        """ """
+        """Test for number_room attribute"""
         new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
+        self.assertNotEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
-        """ """
+        """Test for number_bathrooms attribute"""
         new = self.value()
-        self.assertEqual(type(new.number_bathrooms), int)
+        self.assertNotEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
-        """ """
+        """Test max_guest attribute"""
         new = self.value()
-        self.assertEqual(type(new.max_guest), int)
+        self.assertNotEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
-        """ """
+        """Test for price_by_night attribute"""
         new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
+        self.assertNotEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
-        """ """
+        """Test for latitude attribute"""
         new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        self.assertNotEqual(type(new.latitude), float)
 
     def test_longitude(self):
-        """ """
+        """Test for longitude attribute"""
         new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        self.assertNotEqual(type(new.latitude), float)
 
     def test_amenity_ids(self):
-        """ """
+        """Test for amenity_ids attribute"""
         new = self.value()
         self.assertEqual(type(new.amenity_ids), list)
+
+    def test_to_dictPlace(self):
+        """
+            test to dict method with Place and the type
+            and content
+        """
+        place = Place()
+        dict_cont = place.to_dict()
+        self.assertEqual(type(dict_cont), dict)
+        for attr in place.__dict__:
+            self.assertTrue("__class__" in dict_cont)
+
+    def test_dict_value(self):
+        """
+            test the returned dictionar values
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        place = Place()
+        dict_con = place.to_dict()
+        self.assertEqual(dict_con["__class__"], "Place")
+        self.assertEqual(type(dict_con["created_at"]), str)
+        self.assertEqual(type(dict_con["updated_at"]), str)
+        self.assertEqual(
+            dict_con["created_at"],
+            place.created_at.strftime(time_format)
+        )
+        self.assertEqual(
+            dict_con["updated_at"],
+            place.updated_at.strftime(time_format))
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,58 @@
 #!/usr/bin/python3
-""" """
+"""Test Case For Review Models"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """Test review class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Init to set up environment for test"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """Test for place_id attribute"""
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertNotEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """Test for user_id attribute"""
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertNotEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """Test for text attribute"""
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertNotEqual(type(new.text), str)
+    
+    def test_to_dictReview(self):
+        """
+            test to dict method with Review and the type
+            and content
+        """
+        review = Review()
+        dict_cont = review.to_dict()
+        self.assertEqual(type(dict_cont), dict)
+        for attr in review.__dict__:
+            self.assertTrue("__class__" in dict_cont)
+
+    def test_dict_value(self):
+        """
+            test the returned dictionary values
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        review = Review()
+        dict_con = review.to_dict()
+        self.assertEqual(dict_con["__class__"], "Review")
+        self.assertEqual(type(dict_con["created_at"]), str)
+        self.assertEqual(type(dict_con["updated_at"]), str)
+        self.assertEqual(
+            dict_con["created_at"],
+            review.created_at.strftime(time_format)
+        )
+        self.assertEqual(
+            dict_con["updated_at"],
+            review.updated_at.strftime(time_format))
