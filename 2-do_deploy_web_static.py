acb0a9f504d2d76e115fee3055bb04b44b86f28d
Philip Opiyo <opiyophilipo@gmail.com>
2024-02-09 11:21:30 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,52 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
+""" Function that compresses a folder """
 from datetime import datetime
 from fabric.api import *
 import shlex
 import os
 
-
-env.hosts = ['35.231.33.237', '34.74.155.163']
+env.hosts = ['52.86.205.42', '54.82.176.167']
 env.user = "ubuntu"
-
 
 def do_deploy(archive_path):
     """ Deploys """
     if not os.path.exists(archive_path):
         return False
+
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
+        # Extracting the filename without path and extension
+        archive_filename = os.path.basename(archive_path)
+        archive_name = shlex.split(archive_filename)[0]
 
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
+        releases_path = f"/data/web_static/releases/{archive_name}/"
+        tmp_path = f"/tmp/{archive_filename}"
 
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
+        # Upload the archive to /tmp/
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+        
+        # Create the releases_path directory
+        run(f"mkdir -p {releases_path}")
+
+        # Extract the archive to releases_path
+        run(f"tar -xzf {tmp_path} -C {releases_path}")
+
+        # Remove the uploaded archive
+        run(f"rm {tmp_path}")
+
+        # Move the contents of web_static to releases_path
+        run(f"mv {releases_path}web_static/* {releases_path}")
+
+        # Remove the now redundant web_static directory
+        run(f"rm -rf {releases_path}web_static")
+
+        # Remove the existing /data/web_static/current
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
+
+        # Create a symbolic link to the new version
+        run(f"ln -s {releases_path} /data/web_static/current")
+
         print("New version deployed!")
         return True
-    except:
+    except Exception as e:
+        print(f"Deployment failed: {e}")
         return False
